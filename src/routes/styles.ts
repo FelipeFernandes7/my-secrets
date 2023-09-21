26362689b0f8b537848e653da15f7979171025df
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-09-21 15:19:12 -0300

    center spinner

--- src/routes/styles.ts
+++ src/routes/styles.ts
@@ -6,4 +6,9 @@ export const SpinnerContainer = styled.div`
   height: 100%;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
 `;
