dcfc3f66432ca1849cf483e1cb143b1ad4a0947f
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-09-28 16:10:22 -0300

    adjust auth provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,67 +1,113 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { ReactNode, createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+import { parseUser } from "../helpers/utils";
 import { auth } from "../services";
 
-interface AuthProviderProps {
+type AuthProps = {
   children: ReactNode;
-}
+};
 
-interface AuthContextType {
-  user: UserProps | null;
-  signed: boolean;
-  loadingAuth: boolean;
-  handleInfoUser: ({ name, email, uid }: UserProps) => void;
-}
-
-interface UserProps {
-  uid: string;
+export type User = {
   name: string | null;
-  email: string | null;
-}
+  avatar: string | null;
+  uid: string;
+};
 
-export const AuthContext = createContext({} as AuthContextType);
+type AuthContextType = {
+  user: User | null;
+  loadingAuth: boolean;
+  signInWithGoogle: () => Promise<void>;
+  signInWithGithub: () => Promise<void>;
+  signIn: (email: string, password: string) => Promise<void>;
+  signUpWithEmailAndPassword: (
+    email: string,
+    password: string,
+    displayName: string
+  ) => Promise<void>;
+  logOut: () => Promise<void>;
+};
 
-export function AuthProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<UserProps | null>(null);
+export const AuthContext = createContext<AuthContextType>(
+  {} as AuthContextType
+);
+
+export function AuthProvider({ children }: AuthProps) {
+  const [user, setUser] = useState<User | null>(null);
   const [loadingAuth, setLoadingAuth] = useState(true);
-
-  const unsub = onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setUser({
-        uid: user.uid,
-        email: user.email,
-        name: user.displayName,
-      });
-      setLoadingAuth(false);
-    } else {
-      setUser(null);
-      setLoadingAuth(false);
-    }
-  });
-
-  function handleInfoUser({ name, email, uid }: UserProps) {
-    setUser({
-      uid,
-      name,
-      email,
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+        setUser({
+          name: displayName,
+          avatar: photoURL,
+          uid,
+        });
+        setLoadingAuth(false);
+      }
     });
+    return () => unsubscribe();
+  }, []);
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  async function signInWithGoogle() {
+    const user = await signInWithPopup(auth, googleProvider);
+    const parsedUser = parseUser(user);
+    setUser(parsedUser);
   }
 
-  useEffect(() => {
-    unsub();
+  async function signInWithGithub() {
+    const user = await signInWithPopup(auth, githubProvider);
+    const parsedUser = parseUser(user);
+    setUser(parsedUser);
+  }
 
-    return () => {
-      unsub();
+  async function signIn(email: string, password: string) {
+    const user = await signInWithEmailAndPassword(auth, email, password);
+    const parsedUser = parseUser(user);
+    setUser(parsedUser);
+  }
+
+  async function signUpWithEmailAndPassword(
+    email: string,
+    password: string,
+    displayName: string
+  ) {
+    const user = await createUserWithEmailAndPassword(auth, email, password);
+    const parsedUser = {
+      name: displayName,
+      avatar: null,
+      uid: user.user.uid,
     };
-  }, []);
+    setUser(parsedUser);
+  }
+
+  const logOut = async () => {
+    await signOut(auth);
+    setUser(null);
+  };
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        signed: !!user,
         loadingAuth,
-        handleInfoUser,
+        signIn,
+        signInWithGoogle,
+        signUpWithEmailAndPassword,
+        logOut,
+        signInWithGithub,
       }}
     >
       {children}
