a4809a3ec09b38bef29b2adcb16aa685cdfc242f
Felipe Fernandes <felipekurosaki123@gmail.com>
2023-10-02 18:38:55 -0300

    adjust

--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -2,7 +2,6 @@ import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
   width: auto;
-  margin-right: 1.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
