7336d4d750cbddf305453a27ae2af56de275097e
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-02-10 12:02:11 -0300

    remove unnecessary code

--- src/constants/header.ts
+++
@@ -1,9 +0,0 @@
-export const profile = {
-  name: "_Felipe Fernandes"
-    .toLowerCase()
-    .split("_")
-    .join("@")
-    .split(" ")
-    .join("_"),
-  img: "https://publicdomainvectors.org/tn_img/abstract-user-flat-4.webp",
-};
